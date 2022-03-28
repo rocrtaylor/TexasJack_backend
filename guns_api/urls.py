e482c6c1ca8ebdecde0b28d0ed872e0db8c9230a
Roc Taylor <rocrtaylor@gmail.com>
2022-03-28 18:51:40 -0500

    gun-list

--- guns_api/urls.py
+++ guns_api/urls.py
@@ -1,8 +1,9 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('/', views.GunList.as_view()),
+  path('/', views.GunList.as_view(), name='gun_list'),
   path('api/guns', views.GunList.as_view(), name='gun_list'),
   path('api/guns/<int:pk>', views.GunDetail.as_view(), name='gun_detail'),
 ]
